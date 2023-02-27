94b0d28cd80b1f29db68e9837da595c0e3aa07d3
jibola <pro.ajibolaojo@gmail.com>
2023-02-27 22:07:42 +0100

    capacitor added

---
+++ android/app/src/main/java/app/paypeer/sol/MainActivity.java
@@ -0,0 +1,5 @@
+package app.paypeer.sol;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.paypeer.sol',
+  appName: 'paypeer',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
